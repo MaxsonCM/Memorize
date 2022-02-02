913041f945704c1ce1679b3791964e9a139cbf3e
Maxson Carlos Maidlinger <max_maidlinger@hotmail.com>
2022-02-02 07:33:31 -0300

    Lecture 2
    implementation of Lecture 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,45 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var emojis = ["🚂", "🚀", "🚁", "🚜", "🚗", "🚌", "🏎", "🚒", "🚛", "🛩", "🛻", "🛶", "🚲", "🏍", "🚓", "🚚", "🚠", "🛸", "🛺", "🚇", "🚝", "🛵"]
-    @State var emojiCount = 4
+    @State var emojiCount = 15
     
     var body: some View {
         VStack{
-            HStack{
-                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
-                    CardView(content: emoji)
-                }
+            ScrollView{
+                LazyVGrid(columns:[GridItem(.adaptive(minimum: 65))]){
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji, isFaceUp: false)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }.foregroundColor(Color.red)
             }
+            Spacer()
             HStack{
                 remove
                 Spacer()
                 add
-            }.padding(.horizontal)
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
-        .foregroundColor(Color.red)
         .padding(.horizontal)
     }
     
     
     var remove: some View{
         Button(action: {
-            emojiCount -= 1
+            if (emojiCount > 1 ) {emojiCount -= 1}
         }, label:{
-            VStack{
-                Text("Remove")
-                Text("Card")
-            }
+            Image(systemName: "minus.circle")
         })
     }
     
     var add: some View {
         Button(action: {
-            emojiCount += 1
+            if ( emojiCount < emojis.count ) { emojiCount += 1 }
         }, label:{
-            VStack{
-                Text("Add")
-                Text("Card")
-            }
+            Image(systemName: "plus.circle")
         })
     }
     
@@ -63,7 +62,7 @@ struct CardView: View {
             
             if isFaceUp {
                 shape.fill().foregroundColor(.white)
-                shape.stroke(lineWidth: 3)
+                shape.strokeBorder(lineWidth: 3)
                 Text(content).font(.largeTitle)
             }else{
                 shape.fill()
